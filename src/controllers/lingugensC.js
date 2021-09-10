aac7d9bd104823bc40822fdc558d88285945c900
Ismenia Leao <ismenialeao@gmail.com>
2021-09-10 12:18:26 -0300

    chamando o front

--- src/controllers/lingugensC.js
+++ src/controllers/lingugensC.js
@@ -10,7 +10,7 @@ const getAll = async(req, res) =>{
 
 const getAllFront = async(req, res) =>{
     const front = req.params._area
-    const filterFront = await Linguagem.find({_area: new mongoose.Types.ObjectId()})
+    const filterFront = await Linguagem.filter({front})
         if(filterFront ){
             res.json(filterFront)
         }
@@ -23,9 +23,8 @@ const getAllBack = async(req,res) =>{
     const back = await Linguagem.find().populate('area')
     const filterBack = back.filter( qual  => qual.Area.name == "Back-end")
     
-            res.status(404).json(filterBack)
+        res.status(404).json(filterBack)
 }
-
 
 const createLinguagem = async(req, res) =>{
     const qualLinguagem = new Linguagem({
