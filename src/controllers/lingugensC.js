3b0880b7a5546af34c5b7716bb2fcbff91dd5ce9
Ismenia Leao <ismenialeao@gmail.com>
2021-09-10 01:15:06 -0300

    preguiça 5

--- src/controllers/lingugensC.js
+++ src/controllers/lingugensC.js
@@ -10,7 +10,7 @@ const getAll = async(req, res) =>{
 const getAllFront = async(req, res) =>{
     const front = req.params._area
     const filterFront = await Linguagem.find({_area: new mongoose.Types.ObjectId()})
-        if(filterFront){
+        if(filterFront == "area: 61394e4f62dd10a5bee9f0e6"){
             res.json(filterFront)
         }
         else{
@@ -20,7 +20,7 @@ const getAllFront = async(req, res) =>{
 
 const getAllBack = async(req,res) =>{
     const back = req.params.area
-    const FilterBack = await Linguagem.find({_area:  mongoose.Schema.Types.ObjectId()})
+    const FilterBack = await Linguagem.find({_area: new mongoose.Schema.Types.ObjectId()})
         if(FilterBack == "Back-end"){
             res.json(back)
         }
