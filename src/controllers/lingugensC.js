af063317e4c9b5ced0d38a2213b3a6fa6df0a64e
Ismenia Leao <ismenialeao@gmail.com>
2021-09-11 02:45:36 -0300

    CERVEJAAAAAAAAAAAA

--- src/controllers/lingugensC.js
+++ src/controllers/lingugensC.js
@@ -9,7 +9,7 @@ const getAll = async(req, res) =>{
 }
 
 const getAllFront = async(req, res) =>{
-    const front = await Linguagem.find().populate("area", ["name"]).where('area.name').equals("Front-end")
+    const front = await Linguagem.find().populate("area", ["name"]).where('area').equals("Front-end")
     
 
     res.status(200).json(front)
